899cfb0b379ad5ccd4e71334568b9e983db476c7
imstar15 <imstar15@qq.com>
2025-07-15 20:51:26 +0800

    Add the EIP-7702 code.

---
+++ uniswap-smartwallet/src/contract.ts
@@ -0,0 +1,18 @@
+export const abi = [
+  {
+    "type": "function",
+    "name": "initialize",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "pure"
+  },
+  {
+    "type": "function",
+    "name": "ping",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "pure"
+  },
+] as const;
+ 
+export const contractAddress = '0x000000009B1D0aF20D8C6d0A44e162d11F9b8f00';
